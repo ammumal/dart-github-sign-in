dc0d43197d7abcb1ac2bffc0bfce7fcbaba4d4c2
jlee99 <dorothy991019@gmail.com>
2023-04-18 17:22:53 +0900

    chore: bump webview_flutter from 3.0.0 to 4.0.7

--- lib/src/github_sign_in_page.dart
+++ lib/src/github_sign_in_page.dart
@@ -26,10 +26,32 @@ class GitHubSignInPage extends StatefulWidget {
 class _GitHubSignInPageState extends State<GitHubSignInPage> {
   static const String _userAgentMacOSX =
       "Mozilla/5.0 (Macintosh; Intel Mac OS X 11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36";
+  late final WebViewController _controller;
+  final WebViewCookieManager cookieManager = WebViewCookieManager();
 
   @override
   void initState() {
     super.initState();
+    _controller = WebViewController()
+    ..setJavaScriptMode(JavaScriptMode.unrestricted)
+    ..setUserAgent(widget.userAgent ?? _userAgentMacOSX)
+    ..setNavigationDelegate(NavigationDelegate(
+      onPageFinished: (url) {
+        if (url.contains("error=")) {
+          Navigator.of(context).pop(
+            Exception(Uri.parse(url).queryParameters["error"]),
+          );
+        } else if (url.startsWith(widget.redirectUrl)) {
+          Navigator.of(context).pop(
+              url.replaceFirst("${widget.redirectUrl}?code=", "").trim());
+        }
+      },
+    ))
+    ..loadRequest(Uri.parse(widget.url));
+    if (widget.clearCache) {
+      _controller.clearCache();
+      cookieManager.clearCookies();
+    }
   }
 
   @override
@@ -39,28 +61,7 @@ class _GitHubSignInPageState extends State<GitHubSignInPage> {
           title: Text(widget.title),
           centerTitle: widget.centerTitle,
         ),
-        body: WebView(
-          initialUrl: widget.url,
-          userAgent: widget.userAgent ?? _userAgentMacOSX,
-          javascriptMode: JavascriptMode.unrestricted,
-          onWebViewCreated: (WebViewController controller) {
-            if (widget.clearCache) {
-              controller.clearCache();
-              CookieManager manager = CookieManager();
-              manager.clearCookies();
-            }
-          },
-          onPageFinished: (url) {
-            if (url.contains("error=")) {
-              Navigator.of(context).pop(
-                Exception(Uri.parse(url).queryParameters["error"]),
-              );
-            } else if (url.startsWith(widget.redirectUrl)) {
-              Navigator.of(context).pop(
-                  url.replaceFirst("${widget.redirectUrl}?code=", "").trim());
-            }
-          },
-        ));
+        body: WebViewWidget(controller: _controller));
   }
 
   @override
